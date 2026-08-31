bf7e61879ce33eaca4e1013326356d890a90af6b
Javed <javedsai@gmail.com>
2026-08-31 12:15:05 +0530

    Restructure student CRUD API project and added CORS middleware

--- 09-student-CRUD-API-project/crud-api-project/index.js
+++ 09-student-CRUD-API-project/crud-api-project/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import studentRoutes from "./routes/students.routes.js"
 import {connectDB} from "./config/database.js"
 import multer from "multer"
+import cors from "cors"
 const app = express()
 
 // Connect to MongoDB
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.set('view engine', 'ejs')//not required
 app.use(express.static('public'))//not required
+app.use(cors())
 
 // Mount student API routes under /api/students
 app.use('/api/students', studentRoutes)
